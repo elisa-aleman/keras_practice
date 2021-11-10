2400e6368482e13c27233e04c7d6138e8e3b975d
elisa-aleman <elisa.claire.aleman.carreon@gmail.com>
2021-11-10 18:19:14 +0900

    understanding model

--- python/BERT_practice.py
+++ python/BERT_practice.py
@@ -3,6 +3,7 @@ import pathlib
 import re
 import json
 import string
+import numpy as np
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
@@ -43,8 +44,8 @@ eval_path = keras.utils.get_file("eval.json", eval_data_url)
 ##############
 
 class SquadExample:
-	def __init__(self, question, context, start_char_idx, answer_text, all_answers):
-		self.question = question
+    def __init__(self, question, context, start_char_idx, answer_text, all_answers):
+        self.question = question
         self.context = context
         self.start_char_idx = start_char_idx
         self.answer_text = answer_text
@@ -52,12 +53,12 @@ class SquadExample:
         self.skip = False
 
     def preprocess(self):
-    	context = self.context
-    	question = self.question
-    	answer_text = self.answer_text
-    	start_char_idx = self.start_char_idx
+        context = self.context
+        question = self.question
+        answer_text = self.answer_text
+        start_char_idx = self.start_char_idx
 
-    	# Clean context, answer and question
+        # Clean context, answer and question
         context = " ".join(str(context).split())
         question = " ".join(str(question).split())
         answer = " ".join(str(answer_text).split())
@@ -82,11 +83,151 @@ class SquadExample:
             if sum(is_char_in_ans[start:end]) > 0:
                 ans_token_idx.append(idx)
 
-                
+        if len(ans_token_idx) == 0:
+            self.skip = True
+            return
+
+        # Find start and end token index for tokens from answer
+        start_token_idx = ans_token_idx[0]
+        end_token_idx = ans_token_idx[-1]
+
+        # Tokenize question
+        tokenized_question = tokenizer.encode(question)
+
+        # Create inputs
+        input_ids = tokenized_context.ids + tokenized_question.ids[1:]
+        token_type_ids = [0] * len(tokenized_context.ids) + [1] * len(
+            tokenized_question.ids[1:]
+        )
+        attention_mask = [1] * len(input_ids)
+
+        # Pad and create attention masks.
+        # Skip if truncation is needed
+        padding_length = max_len - len(input_ids)
+        if padding_length > 0:  # pad
+            input_ids = input_ids + ([0] * padding_length)
+            attention_mask = attention_mask + ([0] * padding_length)
+            token_type_ids = token_type_ids + ([0] * padding_length)
+        elif padding_length < 0:  # skip
+            self.skip = True
+            return
+
+        self.input_ids = input_ids
+        self.token_type_ids = token_type_ids
+        self.attention_mask = attention_mask
+        self.start_token_idx = start_token_idx
+        self.end_token_idx = end_token_idx
+        self.context_token_to_char = tokenized_context.offsets
 
 
 
+with open(train_path) as f:
+    raw_train_data = json.load(f)
+
+with open(eval_path) as f:
+    raw_eval_data = json.load(f)
 
 
 
-                
+###########
+
+def create_squad_examples(raw_data):
+    squad_examples = []
+    for item in raw_data["data"]:
+        for para in item["paragraphs"]:
+            context = para["context"]
+            for qa in para["qas"]:
+                question = qa["question"]
+                answer_text = qa["answers"][0]["text"]
+                all_answers = [_["text"] for _ in qa["answers"]]
+                start_char_idx = qa["answers"][0]["answer_start"]
+                squad_eg = SquadExample(
+                    question, context, start_char_idx, answer_text, all_answers
+                )
+                squad_eg.preprocess()
+                squad_examples.append(squad_eg)
+    return squad_examples
+
+def create_inputs_targets(squad_examples):
+    dataset_dict = {
+        "input_ids": [],
+        "token_type_ids": [],
+        "attention_mask": [],
+        "start_token_idx": [],
+        "end_token_idx": [],
+    }
+    for item in squad_examples:
+        if item.skip == False:
+            for key in dataset_dict:
+                dataset_dict[key].append(getattr(item, key))
+    for key in dataset_dict:
+        dataset_dict[key] = np.array(dataset_dict[key])
+
+    x = [
+        dataset_dict["input_ids"],
+        dataset_dict["token_type_ids"],
+        dataset_dict["attention_mask"],
+    ]
+    y = [dataset_dict["start_token_idx"], dataset_dict["end_token_idx"]]
+    return x, y
+
+
+##############
+
+train_squad_examples = create_squad_examples(raw_train_data)
+x_train, y_train = create_inputs_targets(train_squad_examples)
+print(f"{len(train_squad_examples)} training points created.")
+
+eval_squad_examples = create_squad_examples(raw_eval_data)
+x_eval, y_eval = create_inputs_targets(eval_squad_examples)
+print(f"{len(eval_squad_examples)} evaluation points created.")
+
+############
+
+### Create the Question-Answering Model using BERT and Functional API
+
+def create_model():
+    ## BERT encoder
+    encoder = TFBertModel.from_pretrained("bert-base-uncased")
+
+    ## QA Model
+    input_ids = layers.Input(shape=(max_len,), dtype=tf.int32)
+    token_type_ids = layers.Input(shape=(max_len,), dtype=tf.int32)
+    attention_mask = layers.Input(shape=(max_len,), dtype=tf.int32)
+    embedding = encoder(
+        input_ids, token_type_ids=token_type_ids, attention_mask=attention_mask
+    )[0]
+
+    start_logits = layers.Dense(1, name="start_logit", use_bias=False)(embedding)
+    start_logits = layers.Flatten()(start_logits)
+
+    end_logits = layers.Dense(1, name="end_logit", use_bias=False)(embedding)
+    end_logits = layers.Flatten()(end_logits)
+
+    start_probs = layers.Activation(keras.activations.softmax)(start_logits)
+    end_probs = layers.Activation(keras.activations.softmax)(end_logits)
+
+    model = keras.Model(
+        inputs=[input_ids, token_type_ids, attention_mask],
+        outputs=[start_probs, end_probs],
+    )
+    loss = keras.losses.SparseCategoricalCrossentropy(from_logits=False)
+    optimizer = keras.optimizers.Adam(lr=5e-5)
+    model.compile(optimizer=optimizer, loss=[loss, loss])
+    return model
+
+use_tpu = True
+if use_tpu:
+    # Create distribution strategy
+    tpu = tf.distribute.cluster_resolver.TPUClusterResolver.connect()
+    strategy = tf.distribute.TPUStrategy(tpu)
+
+    # Create model
+    with strategy.scope():
+        model = create_model()
+else:
+    model = create_model()
+
+model.summary()
+
+
